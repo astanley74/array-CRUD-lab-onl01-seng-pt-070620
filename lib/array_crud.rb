b7953f090a716c5d64f4b3c692e84a788999352b
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-08 20:31:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [sharks, kings, ducks, knights]
+  array = ["sharks", "kings", "ducks", "knights"]
 end
 
 def add_element_to_end_of_array(array, element)
